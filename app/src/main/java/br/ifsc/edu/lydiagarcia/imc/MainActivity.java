f5bfd6577a5bdb326ed8215d9cbb9ad6c0383231
LydiaGarcia03 <lydiagarcia.chan@gmail.com>
2019-09-03 20:56:00 -0300

    Commit sem ScrollVIew e ImageView

--- app/src/main/java/br/ifsc/edu/lydiagarcia/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/lydiagarcia/imc/MainActivity.java
@@ -23,33 +23,23 @@ public class MainActivity extends AppCompatActivity {
         double imc = peso / Math.pow(altura, 2);
 
         TextView imcDisplay = findViewById(R.id.imcEstado);
-        imcDisplay.setText((int) imc);
-
-        ImageView img = findViewById(R.id.img);
 
         String estado;
 
         if(imc <= 18.5){
             estado = "Abaixo do peso";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.sobrepeso));
         } else if(imc >= 18.6 && imc <= 24.9){
             estado = "Normal";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.normal));
         } else if(imc >= 25 && imc <= 29.9) {
             estado = "Levemente acima do peso";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade1));
         } else if(imc >= 30 && imc <= 34.9){
             estado = "Obesidade I";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade1));
         } else if(imc >= 35 && imc < 39.9){
             estado = "Obesidade II";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade2));
         } else{
             estado = "Obesidade III";
-            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade3));
         }
-
-        imcDisplay.setText(estado);
+        imcDisplay.setText((int) imc + " - " + estado);
 
     }
 }
