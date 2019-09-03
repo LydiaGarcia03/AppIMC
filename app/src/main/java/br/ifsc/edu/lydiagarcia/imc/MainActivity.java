c74e17f894807fec2b3dfecd629bd7b5bab1eb11
LydiaGarcia03 <lydiagarcia.chan@gmail.com>
2019-09-03 20:49:47 -0300

    Commit com ScrollVIew e ImageView

--- app/src/main/java/br/ifsc/edu/lydiagarcia/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/lydiagarcia/imc/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +15,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void redirecionar(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+    public void calcularIMC(View view){
+
+        double peso = Double.parseDouble((findViewById(R.id.editPeso)).toString());
+        double altura = Double.parseDouble((findViewById(R.id.editAltura)).toString());
+
+        double imc = peso / Math.pow(altura, 2);
+
+        TextView imcDisplay = findViewById(R.id.imcEstado);
+        imcDisplay.setText((int) imc);
+
+        ImageView img = findViewById(R.id.img);
+
+        String estado;
+
+        if(imc <= 18.5){
+            estado = "Abaixo do peso";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.sobrepeso));
+        } else if(imc >= 18.6 && imc <= 24.9){
+            estado = "Normal";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.normal));
+        } else if(imc >= 25 && imc <= 29.9) {
+            estado = "Levemente acima do peso";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade1));
+        } else if(imc >= 30 && imc <= 34.9){
+            estado = "Obesidade I";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade1));
+        } else if(imc >= 35 && imc < 39.9){
+            estado = "Obesidade II";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade2));
+        } else{
+            estado = "Obesidade III";
+            img.setImageDrawable(getResources().getDrawable(R.drawable.obesidade3));
+        }
+
+        imcDisplay.setText(estado);
+
     }
 }
